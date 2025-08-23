4bbc2a1698ca7b0aa7ca003a619e47e65abd45e3
Avery Estopinal <avery.estopinal@duke.edu>
2025-08-23 14:48:08 -0400

    added api call

---
+++ nhtsa_api_call.py
@@ -0,0 +1,25 @@
+import requests
+
+def get_vehicle_info(vin,year):
+    url = f'https://vpic.nhtsa.dot.gov/api/vehicles/DecodeVin/{vin}?format=json&modelyear={year}'
+    response = requests.get(url);
+    data = response.json()
+
+    for item in data["Results"]:
+        if item["Variable"] in ["Model Year"]:
+            year = item["Value"]
+        if item["Variable"] in ["Make"]:
+            make = item["Value"]
+        if item["Variable"] in ["Model"]:
+            model = item["Value"]
+    # year =
+    # make = 
+    # model = 
+    print(year)
+    print(make)
+    print(model)
+    return year, make, model	
+		
+vin_num = input("What is vin number")
+
+get_vehicle_info(vin_num)
