0b17c489b8b43a24343aae19fa181c535ea09d1a
Avery Estopinal <avery.estopinal@duke.edu>
2025-08-24 20:33:37 -0400

    Added a docstring to explain function and give API information

--- nhtsa_api_call.py
+++ nhtsa_api_call.py
@@ -1,6 +1,11 @@
 import requests
 
 def get_vehicle_info(vin):
+    """
+    This function takes the parameter "vin" and uses the NHTSA Decode VIN API to retrive year, make, and model
+    The API returns a JSON file which is not a dictionary so the file must be searched with a for loop by "Variable"
+    For more API information visit https://vpic.nhtsa.dot.gov/api/
+    """
     url = f'https://vpic.nhtsa.dot.gov/api/vehicles/DecodeVin/{vin}?format=json'
     response = requests.get(url);
     data = response.json()
